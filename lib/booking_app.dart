2196deb7106e09c6eb3b1dcea4a3df7d018f7358
Samar <sam_abdulaziz@yahoo.com>
2022-09-19 19:29:39 +0200

    initial

---
+++ lib/booking_app.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class BookingApp extends StatelessWidget {
+  const BookingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
+import 'package:algoriza_booking_app/booking_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-                primarySwatch: Colors.blue,
-      ),
-    );
-  }
+  runApp( const BookingApp());
 }
