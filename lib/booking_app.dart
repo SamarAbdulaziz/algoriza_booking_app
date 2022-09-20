c54e235c29407839c091a6aaa73c922df5ee4e76
Samar <sam_abdulaziz@yahoo.com>
2022-09-20 19:31:39 +0200

    app routers add on config folder

--- lib/booking_app.dart
+++ lib/booking_app.dart
@@ -1,8 +1,8 @@
+import 'package:algoriza_booking_app/config/routes/app_routes.dart';
 import 'package:algoriza_booking_app/core/services/service_locator.dart';
 import 'package:algoriza_booking_app/feature/auth/presentaion/controllers/auth_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'feature/auth/presentaion/screens/login_screen.dart';
 
 class BookingApp extends StatelessWidget {
   const BookingApp({Key? key}) : super(key: key);
@@ -11,8 +11,9 @@ class BookingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider<AuthenticationCubit>(
       create: (context) => sl<AuthenticationCubit>(),
-      child: MaterialApp(
-        home: LoginScreen(),
+      child: const MaterialApp(
+        initialRoute: Routes.initialRoute,
+        onGenerateRoute: AppRoutes.onGenerateRoute,
       ),
     );
   }
