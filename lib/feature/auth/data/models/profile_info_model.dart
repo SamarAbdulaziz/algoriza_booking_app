90f18cb0153d749dfc71523799447a286e38bda4
Samar <sam_abdulaziz@yahoo.com>
2022-09-21 17:46:04 +0200

    Profile info model added by samar

---
+++ lib/feature/auth/data/models/profile_info_model.dart
@@ -0,0 +1,22 @@
+import 'package:algoriza_booking_app/feature/auth/data/models/data_model.dart';
+import 'package:algoriza_booking_app/feature/auth/domain/entities/data.dart';
+import 'package:algoriza_booking_app/feature/auth/domain/entities/profile-info.dart';
+
+class ProfileInfoModel extends ProfileInfo {
+
+
+  const ProfileInfoModel({
+    required String statusType,
+    required Data data,
+  }):super(
+    statusType: statusType,
+    data: data,
+  );
+
+  factory ProfileInfoModel.fromJson(Map<String, dynamic> json) {
+    return ProfileInfoModel(
+      statusType: json['status']['type'],
+      data: DataModel.fromJson(json['data']),
+    );
+  }
+}
