f02c1e84bf3c2756f0c562fa8cf8452a4c5b3d92
AlaaOsama25 <alaaosamazaki@gmail.com>
2022-09-24 19:26:55 +0200

    getstarted & editScreens

--- lib/feature/auth/presentation/screens/get_started_screen.dart
+++ lib/feature/auth/presentation/screens/get_started_screen.dart
@@ -8,20 +8,65 @@ class GetStartedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.grey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            DefaultButton(
-              title: 'Get Started',
-              ontap: () {
-                Navigator.pushNamed(context, Routes.onBoardingRoute);
-              },
+        body:
+        Column(children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/GetStarted.jpg'),
+                      fit: BoxFit.cover
+                  )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Motel',
+                            style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold,),),
+                          SizedBox(height: 20,),
+                          Text('Best hotel deals for your holiday', style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w400,wordSpacing: 1),),
+                          SizedBox(height: 170,),
+                        ],
+                      ),
+                    ),
+                    DefaultButton(
+                      title: 'Get Started',
+                      ontap: () {
+                        Navigator.pushNamed(context, Routes.onBoardingRoute);
+                      },
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Already have account?',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w400),),
+                        TextButton(
+                          child:Text('Login',
+                            style: TextStyle(
+                              color: Colors.teal.shade200,
+                              fontWeight: FontWeight.bold,),),
+                          onPressed: () {
+                            Navigator.pushNamed(context, Routes.loginScreenRoute);
+                          },
+                        ),
+
+                      ],
+                    )
+                  ],),
+              ),
+
             ),
-          ],
-        ),
-      ),
+          ),
+        ]
+        )
     );
   }
 }
