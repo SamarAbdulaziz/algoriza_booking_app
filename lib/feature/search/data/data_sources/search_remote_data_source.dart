42e860ff58dc68380d27dbdc039d35515a0de591
Samar <sam_abdulaziz@yahoo.com>
2022-09-25 02:55:29 +0200

    search feature data layer is done approximatly

---
+++ lib/feature/search/data/data_sources/search_remote_data_source.dart
@@ -0,0 +1,58 @@
+import 'package:algoriza_booking_app/core/errors/exceptions.dart';
+import 'package:algoriza_booking_app/core/utiles/constants.dart';
+import 'package:algoriza_booking_app/feature/search/data/models/search_data_model.dart';
+import 'package:dio/dio.dart';
+
+abstract class BaseSearchRemoteDataSource {
+  Future<SearchDataModel> search({
+    String? name,
+    String? address,
+    int? maxPrice,
+    int? minPrice,
+    int? facilities,
+    double? lat,
+    double? long,
+    double? distance,
+    int? count,
+    int? page,
+  });
+}
+
+class SearchRemoteDataSource extends BaseSearchRemoteDataSource {
+  var dio = Dio(BaseOptions(
+    baseUrl: AppConstants.baseUrl,
+    receiveDataWhenStatusError: true,
+    connectTimeout: 5000,
+  ));
+
+  @override
+  Future<SearchDataModel> search(
+      {String? name,
+      String? address,
+      int? maxPrice,
+      int? minPrice,
+      int? facilities,
+      double? lat,
+      double? long,
+      double? distance,
+      int? count,
+      int? page}) async {
+    final response = await dio.get(AppConstants.searchPath, queryParameters: {
+      'name': name,
+      'address': address,
+      'max_price': maxPrice,
+      'min_price': minPrice,
+      'facilities[0]': facilities,
+      'latitude': lat,
+      'longitude': long,
+      'distance': distance,
+      'count': count,
+      'page': page
+    });
+    if (response.statusCode == 200) {
+      return SearchDataModel.fromJson(response.data);
+    } else {
+      throw ServerException('Server Exception');
+    }
+  }
+}
